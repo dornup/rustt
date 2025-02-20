c7ed94388e6e0b317cb2b6e431b39f9c5b55f756
Melany Dorn <mdiscool1@gmail.com>
2025-02-20 19:17:52 +0700

    1

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,21 @@
+use itertools::Itertools;
+
+fn fake_bin(s: &str) -> String {
+    s.chars().map(|c|match c {
+        '0'..='4'=>'0',
+        _ => '1'
+    }).collect()
+}
+
+fn spacify(s: &str) -> String {
+    s.chars().join(" ")
+}
+
 fn main() {
-    let x = 50;
-
-    /*
-    match x {
-        10 => println!("{:?}", 10),
-        0..=50 => println!("{:?}", 5), // .. - невключительно ..= включительно
-        _ => println!("{:?}", 0), // _ - все оставшиеся случаи
-    }
-    */
-
-    if x > 20 {
-        println!("{:?}", 20)
-    } else if x > 10{
-        println!("{:?}", 0)
-    } else {
-        println!("{:?}", 1)
-    }
-
-    for i in (0..5).rev().step_by(2) {
-        println!("{:?}", i)
-    }
-
-    let s = "abc";
-    println!("{:?}", s.to_uppercase())
+    println!("{:?}", 2_i32.pow(10));
+    let x = vec![1,2,3]
+    .into_iter()
+    .fold(1, |acc, i| {acc * i});
+    // .collect::<Vec<i32>>()
+    println!("{:?}", x)
 }
