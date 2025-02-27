89a73ba00c0e7165ed4ad01b0d095c83e38db610
Melany Dorn <mdiscool1@gmail.com>
2025-02-27 19:13:29 +0700

    rrr

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,30 @@
 use itertools::Itertools;
 
-fn fake_bin(s: &str) -> String {
-    s.chars().map(|c|match c {
-        '0'..='4'=>'0',
-        _ => '1'
-    }).collect()
-}
-
-fn spacify(s: &str) -> String {
-    s.chars().join(" ")
-}
-
 fn main() {
-    println!("{:?}", 2_i32.pow(10));
-    let x = vec![1,2,3]
-    .into_iter()
-    .fold(1, |acc, i| {acc * i});
-    // .collect::<Vec<i32>>()
-    println!("{:?}", x)
+    let mut m = [1, 2, 3];
+    m = m.map(|elem| elem+1);
+    println!("{:?}", m)
+
+    let k = m.iter().fold(1, |acc, e| acc * e);
+    println!("{:?}", k)
 }
+
+fn generate_range(start: usize, stop: usize, step: usize) -> Vec<usize> {
+    (start..=stop).step_by(step).collect()
+}
+
+fn name_shuffler(s: &str) -> String {
+    println!("{:?}", s.split_ascii_whitespace().rev().join(" "));
+    // s.rsplit(' ').join(" ")
+    String::new()
+}
+
+fn multi_table(n: u64) -> String {
+    (1..=10).map(|c|format!("{} * {} = {}", c, n, c*n)).join("\n")
+}
+
+fn sort_numbers(arr: &Vec<i32>) -> Vec<i32> {
+    let mut ans = arr.clone();
+    ans.sort_unstable();
+    ans
+  }
